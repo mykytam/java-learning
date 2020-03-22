1dd89d04f99736fd604750771f85fc0cadf3ba13
mykytam <morar.nikita@gmail.com>
2020-03-22 17:01:12 +0200

    Custom Annotation

---
+++ annotations/Test.java
@@ -0,0 +1,7 @@
+
+public class Test {
+    @MethodInfo(author = "Morar", dateOfCreation = 2020, purpose = "Print Hello World")
+    public void printHelloWorld() {
+        System.out.println("Hello world");
+    }
+}
