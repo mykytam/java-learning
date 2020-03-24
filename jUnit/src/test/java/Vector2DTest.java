856952971695e7f66969d5e161ea363c1a1e49d0
mykytam <morar.nikita@gmail.com>
2020-03-24 11:15:22 +0200

    JUnit

--- jUnit/src/test/java/Vector2DTest.java
+++ jUnit/src/test/java/Vector2DTest.java
@@ -1,24 +1,27 @@
 import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class Vector2DTest {
+    private final double EPS = 1e-9;
+    private static Vector2D v1;
 
+    @BeforeClass // executed once
+    public static void createNewVector() {
+        v1 = new Vector2D(); // same for every test
+    }
     @Test
     public void newVectorShouldHaveZeroLength() {
-        Vector2D v1 = new Vector2D(); // action
-        // assertion
-        Assert.assertEquals(0, v1.length(), 1e-9); // expected - actual - delta
+        Assert.assertEquals(0, v1.length(), EPS); // expected - actual - delta
     }
 
     @Test
     public void newVectorShouldHaveZeroX() {
-        Vector2D v1 = new Vector2D();
-        Assert.assertEquals(0, v1.getX(), 1e-9);
+        Assert.assertEquals(0, v1.getX(), EPS);
     }
 
     @Test
     public void newVectorShouldHaveZeroY() {
-        Vector2D v1 = new Vector2D();
-        Assert.assertEquals(0, v1.getY(), 1e-9);
+        Assert.assertEquals(0, v1.getY(), EPS);
     }
 }
